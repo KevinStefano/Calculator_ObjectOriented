a2a82d4157bc081b2865f25ebb40f333da0ac64f
stanleyyoga123 <yoga360227@gmail.com>
2020-03-21 18:40:13 +0700

    add test

---
+++ Calculator_2/test/parser/ParserTest.java
@@ -0,0 +1,24 @@
+package parser;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ParserTest {
+
+	@Test
+	public void testParser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetOperation() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testParsing() {
+		fail("Not yet implemented");
+	}
+
+}
